6f807eb603a467c2877e6f079b89848f2d05638d
Marco André Mendes <marcoandre@gmail.com>
2024-10-10 11:10:36 -0300

    Criação da API para Categoria.

--- app/urls.py
+++ app/urls.py
@@ -9,10 +9,11 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, CategoriaViewSet
 
 router = DefaultRouter()
 
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 
 urlpatterns = [
--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100, verbose_name="descrição", help_text="Prateleira do livro")
 
+    def __str__(self):
+        return f"({self.id}) {self.descricao}"
